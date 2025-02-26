2c0263305a9a8e8a6f1363676a8867b65a282212
Gehad Abd-elhakeem Gamal <106842051+gehadgady@users.noreply.github.com>
2025-02-26 21:11:02 +0200

    Add files via upload

---
+++ streamlet/windApp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+st.title('Wind Turbine Power Prediction App')
+st.write('This is a simple example of a Streamlit app that shows some data analysis on the Wind Turbine dataset.')
+
